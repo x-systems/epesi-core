232d5effbdde4360821932070410227574b80aa3
Georgi Hristov <ghristov@gmx.de>
2019-11-05 15:59:23 +0100

    [UPDATE] launch button

--- src/System/Seeds/LaunchButton.php
+++ src/System/Seeds/LaunchButton.php
@@ -4,6 +4,10 @@ namespace Epesi\Core\System\Seeds;
 
 class LaunchButton extends ActionButton
 {
+	public $defaultTemplate = 'launch_button.html';
+	
     public $ui = 'basic launch pjax button';
+    
+    public $container = 'View';
   
 }
