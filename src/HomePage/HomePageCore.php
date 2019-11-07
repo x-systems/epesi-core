3de6ff25e05b5160adcbc4f285e8fe8281feebd5
Georgi Hristov <ghristov@gmx.de>
2019-11-07 01:20:21 +0100

    [UPDATE] add default home pages on install

--- src/HomePage/HomePageCore.php
+++ src/HomePage/HomePageCore.php
@@ -4,6 +4,7 @@ namespace Epesi\Core\HomePage;
 
 use Epesi\Core\System\Integration\Modules\ModuleCore;
 use Epesi\Core\System\User\Access\AccessCore;
+use Epesi\Core\HomePage\Database\Models\HomePage;
 
 class HomePageCore extends ModuleCore
 {
@@ -21,7 +22,16 @@ class HomePageCore extends ModuleCore
 	
 	public function install()
 	{
+		// setup default home pages
+		HomePage::create([
+				'path' => 'view/dashboard',
+				'role' => 'Super Admin'
+		]);
 		
+		HomePage::create([
+				'path' => 'view/dashboard',
+				'role' => 'Employee'
+		]);
 	}
 	
 	public function uninstall()
